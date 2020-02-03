c869629b63a0d1cc4df927c1daf9e3772bf4448c
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-03 21:11:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
-  @@artist = []
-  @@genre = []
+  @@artists = []
+  @@genres = []
   
  def initialize(song_name,artist,genre)
     @name = song_name
